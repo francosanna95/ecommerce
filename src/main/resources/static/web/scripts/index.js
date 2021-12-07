9ad43d65a0644bb9d31761988ae9ea4e710d8b04
cmfages <you@example.com>
2021-12-06 21:55:31 -0300

    Forms terminados (faltan v-models)

---
+++ src/main/resources/static/web/scripts/index.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data() {
+        return {
+
+        }
+    },
+    created() {
+
+    },
+    methods: {
+    
+    },
+    computed: {
+    }
+
+})
+app.mount("#app");
