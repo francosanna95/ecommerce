1496df07e09127da5c4a68402266e873b66e70e0
francosanna95 <francosanna39073312@gmail.com>
2021-12-07 11:17:04 -0300

    mergingh

--- src/main/resources/static/web/scripts/admin.js
+++ src/main/resources/static/web/scripts/admin.js
@@ -23,25 +23,59 @@ const app = Vue.createApp({
     newTicket(){
         axios({
             method:'POST',
-            url:`/api/products/tickets?agencyName=Babel`,
+            url:`/api/products/tickets`,
             data:{
                      "price":1500,
                      "disscountCode":"asd123",
-                     "productId":15,
-                     "points":200,
-                     "address":"Av. siempreviva 123",
-                     "name":"Los Simpson Hotel",
-                     "maxCapacity":40,
-                     "stock":40,
+                     "description" : "This is a great ticket, the one you need",
+                     "productName":"El mejor vuelo",
+                     "stock":250,
                      "imgUrl":"IMG url",
-                     "origin":"cordoba",
-                     "destination":"bsAs",
-                     "departureDate": "2015-08-04T10:11:30",
-                     "arrivalDate": "2015-08-04T10:11:30"
-                 }
-            })
+                     "address":"Aeropuerto Internacional de Ezeiza",
+                     "departureTime": "2015-08-04T10:11:30",
+                     "arrivalTime" : "2015-08-04T10:11:30",
+                     "departureLocation" : "Buenos Aires",
+                     "arrivalLocation" : "Bariloche",
+                     "agencyName" : "Babel"
+            }
+        })
     },
-
+    newHotel(){
+        axios({
+             method:'POST',
+             url:`/api/products/hotels`,
+             data:{
+                 "price":1500,
+                 "disscountCode":"asd123",
+                 "description":"the new Franco's inn!",
+                 "productName":"Los Simpson Hotel",
+                 "stock":40,
+                 "imgUrl":"IMG url",
+                 "address":"Av. siempreviva 123",
+                 "parking":false,
+                 "concierge":true,
+                 "agencyName" : "Babel"
+             }
+        })
+    },
+    newEvent(){
+        axios({
+            method:'POST',
+            url:`/api/products/events`,
+            data:{
+                "price":1500,
+                "disscountCode":"asd123",
+                "description" : "Maluma Baby",
+                "productName":"The new Maluma experience",
+                "stock":250,
+                "imgUrl":"IMG url",
+                "address":"Aeropuerto Internacional de Ezeiza",
+                "agencyName" : "Babel",
+                "artist":"Maluma",
+                "vip":true
+            }
+        })
+    },
     logout() {
       axios.post('/api/logout')
         .then(response => {
