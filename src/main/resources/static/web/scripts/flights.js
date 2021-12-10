b4beb316b2deea57dfda13df1a747899befca6c4
francosanna95 <francosanna39073312@gmail.com>
2021-12-10 17:09:57 -0300

    mejoramos el carrito

--- src/main/resources/static/web/scripts/flights.js
+++ src/main/resources/static/web/scripts/flights.js
@@ -21,17 +21,13 @@ const app = Vue.createApp({
             return error.message;
         })
          if (sessionStorage.getItem('cart')) {
-               try {
-                    console.log(this.cart);
-                    this.cart = JSON.parse(sessionStorage.getItem('cart'));
-                            console.log(this.cart);
-                        } catch (e) {
-                            sessionStorage.removeItem('cart');
-                        }
-                        this.cart=sessionStorage.getItem('cart')
-                        console.log(this.cart)
-
-                    }
+                   try {
+                      this.cart = JSON.parse(sessionStorage.getItem('cart'));
+                   } catch (e) {
+                       sessionStorage.removeItem('cart');
+                     }
+                   console.log(this.cart)
+               };
     },
     mounted() {
         },
@@ -47,23 +43,28 @@ const app = Vue.createApp({
         addToCart(ticket){
         console.log(ticket.productId);
             axios.post("/api/clients/current/addToCart/ticket",`ticketId=${ticket.productId}&clase=${this.clase}&passengers=${this.passengers}`)
-            .then(resp=>{
-                ticket=resp.data;
-                console.log(ticket);
-                if(ticket.stock<=0){
-                   alert("No stock");}
-                   else{
-                     ticket.stock--;
-                    console.log(this.cart);
-                     if(this.cart.some(prod=>prod.productId==ticket.productId)){
-                       ticket.quantity=ticket.quantity+this.passengers;}
-                     else{ticket.quantity=this.passengers;}
-                     ticket.clase=this.clase;
-                ticket.subtotal=ticket.quantity*ticket.price;
-                this.cart.push(ticket);
-                console.log(this.cart);
-                this.savingCart();}
-            })
+               .then(resp => {
+                                ticket = resp.data;
+                                console.log(ticket);
+                                if (ticket.stock <= 0) {
+                                    alert("No stock");
+                                } else {
+                                    ticket.stock--;
+                                    console.log(this.cart);
+                                    if (this.cart.some(prod => prod.productId == ticket.productId)) {
+                                        let id = this.cart.findIndex(prod => prod.productId == ticket.productId);
+                                        //actualizar cantidad en ese producto
+
+                                        this.cart[id].quantity= ticket.quantity;
+                                    } else {
+                                        ticket.quantity = this.passengers;
+                                        ticket.clase = this.clase;
+                                        ticket.subtotal = ticket.quantity * ticket.price;
+                                        this.cart.push(ticket);
+                                        console.log(this.cart);
+                                    }
+                                    this.savingCart();
+            }})
             .catch(err=> console.log(err));
         },
         deleteCartObject(ticket){
